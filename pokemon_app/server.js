b0c49b7291f68ba7f0cbca953f7bf8b733dd9864
JudeJeanPierre <110211488+JudeJeanPierre@users.noreply.github.com>
2022-09-17 12:56:04 -0500

    2nd commit

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,10 +1,18 @@
 const express = require ('express')
 const app = express()
 const PORT = 3000
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
 app.get('/', (req, res) =>{
     res.send('Welcome to the Pokeman App!');
 })
+
+app.get('/pokemon/', (req, res) =>{
+    res.render('Index.jsx')
+})
+
+
 
 
 app.listen(PORT, (req, res) =>{
